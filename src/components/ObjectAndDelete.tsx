d3acafa878be234c479b15a24ecbb1a3f018d10e
AndyLy00 <andreia.cristina.siretanu@gmail.com>
2023-01-02 10:36:01 +0200

    Update ObjectAndDelete.tsx

--- src/components/ObjectAndDelete.tsx
+++ src/components/ObjectAndDelete.tsx
@@ -2,10 +2,11 @@ import React, {MutableRefObject, useEffect, useRef, useState} from 'react';
 import {Button, Layout} from "antd";
 import {CloseOutlined} from "@ant-design/icons";
 import apiClient from "../axios/data";
+// import axios from "axios";
 
 const ObjectAndDelete = () => {
     const [persons, setPerson] = useState<any>([]);
-    const delete_id = useRef() as MutableRefObject<HTMLInputElement>;
+    const delete_id = useRef() as MutableRefObject<HTMLButtonElement>;
 
     const fetchData = async () => {
         const {data} = await apiClient.get("/")
@@ -18,24 +19,16 @@ const ObjectAndDelete = () => {
 
     async function deleteDataById() {
         const id = delete_id.current.value;
-        //
-        // if (id) {
-        //   try {
-        //     const res = await axios.delete(`https://retoolapi.dev/geeOvB/data?id=${id}`);
-        //     console.log(res);
-        //   } catch (err) {
-        //     console.log(err);
-        //   }
-        // }
-        fetch(`https://retoolapi.dev/geeOvB/data?id=${id}`, {method: 'DELETE'})
-            .then(() => console.log("Deleted"));
-    }
+
+            fetch(`http://localhost:3001/data/${id}`, {method: 'DELETE'})
+                .then(() => console.log("Deleted"));
+        }
 
     return (
         <Layout style={{backgroundColor: "#bae0ff", width: "100%", maxWidth: "700px", margin: "0 auto"}}>
 
             {persons.map((person: { id: number; Job: string; City: string; Name: string; Email: string; DateCreated: Date; "Phone Number": string; }) => (
-                <div className="person_block" key={person.id} ref={delete_id}>
+                <div className="person_block" key={person.id}>
                     <div style={{display: "flex"}}>
                         <div className="person_info"> {person.Name} </div>
                         <div className="person_info"> {person.Job} </div>
@@ -48,7 +41,7 @@ const ObjectAndDelete = () => {
                         <div className="person_info"> {person.Email} </div>
                         <div className="person_info"> {person["Phone Number"]} </div>
                     </div>
-                    <Button className="close_button" onClick={deleteDataById}> <CloseOutlined/> </Button>
+                    <Button className="close_button" ref={delete_id} value={person.id} onClick={deleteDataById}> <CloseOutlined/> </Button>
                 </div>
             ))}
         </Layout>
