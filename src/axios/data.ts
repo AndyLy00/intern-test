7e98be15f6f3889650eeaee3618fff53863a8d73
AndyLy00 <andreia.cristina.siretanu@gmail.com>
2023-01-02 10:34:17 +0200

    Update data.ts

--- src/axios/data.ts
+++ src/axios/data.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://retoolapi.dev/geeOvB/data",
+    // baseURL: "https://retoolapi.dev/geeOvB/data",
+    baseURL: "http://localhost:3001/data",
     headers: {
         "Content-type": "application/json"
     }
